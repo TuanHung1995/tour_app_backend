464717b32e5e1d2cab45c9c40ee1649984ba9fd0
TuanHung1995 <tuanhungg1995@gmail.com>
2024-09-17 11:42:14 +0700

    add category

---
+++ src/main/java/com/springboot/mvc/tour_app/entity/Category.java
@@ -0,0 +1,26 @@
+package com.springboot.mvc.tour_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
